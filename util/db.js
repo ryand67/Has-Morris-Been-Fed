d78c884f150009e6187801e815c6bdf83102fe4a
ryand67 <rwdevenney@gmail.com>
2021-02-05 21:05:01 -0500

    env

--- util/db.js
+++ util/db.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const client = new MongoClient(process.env.DATABASE_URL, {
+const client = new MongoClient(process.env.NEXT_PUBLIC_DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
